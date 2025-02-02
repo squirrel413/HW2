9d6a2d6177224a92664eb0d1dcd8f028affb0f56
squirrel413 <nstibbels@gmail.com>
2025-02-02 01:40:52 -0500

    Upload

--- src/game/Lines.java
+++ src/game/Lines.java
@@ -1,22 +1,35 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Lines {
     //Field variables
     private final int rows;
     private final int columns;
     private Dot[][] dots;
+    private static final ArrayList<Line> lines;
 
     //Constructor
     public Lines(int rows, int columns, Dot[][] dots) {
         this.rows = rows;
         this.columns = columns;
         this.dots = dots;
+        for (int i = 0; i < (rows-1); i++) {
+            for (int j = 0; j < columns; j++) {
+                lines.add(new Line(dots[i][j], dots[i + 1][j]));
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < (columns-1); j++) {
+                lines.add(new Line(dots[i][j], dots[i][j+1]));
+            }
+        }
     }
 
     //Methods
     public Line getLine(int row1, int column1, int row2, int column2) {
         if (row1 <= row2 && column1 <= column2)
-            return new Line(dots[row1][column1], dots[row2][column2]);
+            return ;
         else
             return null;
     }
